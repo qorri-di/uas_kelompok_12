4baf3d9fda65ac29efc6041885d1f8212a0716e3
Mr Esbatu <qorri24j@gmail.com>
2025-07-23 00:46:22 +0700

    fixing

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+import threading
+
 from app import create_app
+from app.services import run_telegram_bot
 
 app = create_app()
 
 if __name__ == '__main__':
+    # Jalankan telegram bot di thread terpisah
+    bot_thread = threading.Thread(target=run_telegram_bot, daemon=True)
+    bot_thread.start()
+
     app.run(debug=True)
