5657caf8cbee92ceb67484dff0bc9d0ef407e580
Mr Esbatu <qorri24j@gmail.com>
2025-07-22 22:52:12 +0700

    fixing

--- app/config/telegram.py
+++ app/config/telegram.py
@@ -6,13 +6,17 @@ load_dotenv()
 
 # Ambil token dari .env
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-WEBHOOK_URL = os.getenv("TELEGRAM_WEBHOOK_URL")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL_BASE")
+WEBHOOK_PATH = os.getenv("WEBHOOK_URL_PATH")
 
 if not TELEGRAM_BOT_TOKEN:
     raise ValueError("TELEGRAM_BOT_TOKEN tidak ditemukan di file .env")
 
 if not WEBHOOK_URL:
-    raise ValueError("TELEGRAM_WEBHOOK_URL tidak ditemukan di file .env")
+    raise ValueError("WEBHOOK_URL_BASE tidak ditemukan di file .env")
+
+if not WEBHOOK_PATH:
+    raise ValueError("WEBHOOK_URL_PATH tidak ditemukan di file .env")
 
 # Format endpoint webhook Telegram
-WEBHOOK_ENDPOINT = f"{WEBHOOK_URL}/webhook"
+WEBHOOK_ENDPOINT = f"{WEBHOOK_URL}{WEBHOOK_PATH}"
