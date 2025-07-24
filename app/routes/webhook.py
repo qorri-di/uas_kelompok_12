64d2eaac4e843df288b314c7279ced6c0ee1ed33
Mr Esbatu <qorri24j@gmail.com>
2025-07-25 01:16:01 +0700

    fixing

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
-from telegram import Update
-from app.services.telegram_service import app_bot
+# from telegram import Update
+# from app.services.telegram_service import app_bot
 
 webhook_bp = Blueprint("webhook", __name__)
 
@@ -8,6 +8,6 @@ webhook_bp = Blueprint("webhook", __name__)
 @webhook_bp.route("/webhook", methods=["POST"])
 def telegram_webhook():
     if request.method == "POST":
-        update = Update.de_json(request.get_json(force=True), app_bot.bot)
-        app_bot.update_queue.put_nowait(update)
+        # update = Update.de_json(request.get_json(force=True), app_bot.bot)
+        # app_bot.update_queue.put_nowait(update)
         return "OK", 200
