a0c534c171859483cf338f3503731da417c05953
Mr Esbatu <qorri24j@gmail.com>
2025-07-25 01:15:12 +0700

    fixing

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 import os
 from flask import Flask
 from app.config.database import init_mysql
-# import asyncio
-# from app.config.telegram import WEBHOOK_ENDPOINT
-# from app.services.telegram_service import app_bot, run_telegram_bot
+
+app = Flask(__name__)
 
 
 def create_app():
-    app = Flask(__name__)
     app.secret_key = os.getenv('SECRET_KEY', 'defaultsecret')
 
     # Inisialisasi database
@@ -19,14 +17,5 @@ def create_app():
     app.register_blueprint(dashboard.dashboard_bp)
     app.register_blueprint(otp.otp_bp)
     app.register_blueprint(webhook.webhook_bp)
-
-    # Atur handler Telegram
-    # run_telegram_bot()
-
-    # Set webhook Telegram
-    # async def set_webhook():
-    #     await app_bot.bot.set_webhook(url=WEBHOOK_ENDPOINT)
-    #
-    # asyncio.run(set_webhook())
 
     return app
