f4e2be3a36241dba30aebae3462b7d01d3e3e71b
Mr Esbatu <qorri24j@gmail.com>
2025-07-25 01:15:45 +0700

    fixing

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     bot_thread = threading.Thread(target=run_telegram_bot, daemon=True)
     bot_thread.start()
 
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
